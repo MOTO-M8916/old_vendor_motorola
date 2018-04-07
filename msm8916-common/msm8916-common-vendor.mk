1c77c6bfda6336ba98cfd93ec934d8a49e809800
MatiHalperin <mati@cypheros.co>
2018-04-07 14:33:13 +0000

    msm8916-common: Move some binaries to vendor

--- msm8916-common/msm8916-common-vendor.mk
+++ msm8916-common/msm8916-common-vendor.mk
@@ -17,14 +17,14 @@
 
 PRODUCT_COPY_FILES += \
     vendor/motorola/msm8916-common/proprietary/bin/adsprpcd:system/bin/adsprpcd \
-    vendor/motorola/msm8916-common/proprietary/bin/irsc_util:system/bin/irsc_util \
-    vendor/motorola/msm8916-common/proprietary/bin/netmgrd:system/bin/netmgrd \
+    vendor/motorola/msm8916-common/proprietary/bin/irsc_util:system/vendor/bin/irsc_util \
+    vendor/motorola/msm8916-common/proprietary/bin/netmgrd:system/vendor/bin/netmgrd \
     vendor/motorola/msm8916-common/proprietary/bin/qmi_motext_hook:system/bin/qmi_motext_hook \
-    vendor/motorola/msm8916-common/proprietary/bin/qmuxd:system/bin/qmuxd \
-    vendor/motorola/msm8916-common/proprietary/bin/qseecomd:system/bin/qseecomd \
+    vendor/motorola/msm8916-common/proprietary/bin/qmuxd:system/vendor/bin/qmuxd \
+    vendor/motorola/msm8916-common/proprietary/bin/qseecomd:system/vendor/bin/qseecomd \
     vendor/motorola/msm8916-common/proprietary/bin/radish:system/bin/radish \
-    vendor/motorola/msm8916-common/proprietary/bin/rmt_storage:system/bin/rmt_storage \
-    vendor/motorola/msm8916-common/proprietary/bin/time_daemon:system/bin/time_daemon \
+    vendor/motorola/msm8916-common/proprietary/bin/rmt_storage:system/vendor/bin/rmt_storage \
+    vendor/motorola/msm8916-common/proprietary/bin/time_daemon:system/vendor/bin/time_daemon \
     vendor/motorola/msm8916-common/proprietary/etc/data/dsi_config.xml:system/etc/data/dsi_config.xml \
     vendor/motorola/msm8916-common/proprietary/etc/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     vendor/motorola/msm8916-common/proprietary/etc/data/qmi_config.xml:system/etc/data/qmi_config.xml \
